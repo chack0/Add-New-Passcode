c0fd296aa642756e5bf1d63aaa50df90f06bb385
chack0 <41887600+chack0@users.noreply.github.com>
2019-11-12 18:28:52 +0530

    Changed imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:createpasscodepage/enterPasscode.dart';
 import 'package:flutter/material.dart';
-import 'checkPassword.dart';
+import 'enterPasscode.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,18 +12,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: CheckPassword(),
+      home: EnterPasscode(),
     );
   }
 }
